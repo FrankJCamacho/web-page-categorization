8248007c09d1a648bb6928b5a41a202b5ebfc17e
Franklin Camacho III <60351478+FrankJCamacho@users.noreply.github.com>
2022-10-18 17:39:07 -0400

    Updated Similarity class
    Fixed methods by utilizing the space variable instead of the table's length, changed double idf's Hash[] to Hash.
    Added a check in the idf method to make sure program isn't dividing by 0.

--- CSC365-P1/src/project/Similarity.java
+++ CSC365-P1/src/project/Similarity.java
@@ -1,35 +1,38 @@
 package project;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Similarity {
-
-    public double similarity(ArrayList<String>[] user, String term) {
+    //this method is for the user provided page
+    public double similarity(Hash user, String term) {
         double result = 0;
-        for (int i = 0; i < user.length; i++) {
-            for(int k = 0; k < user[i].size(); k++) {
-                String key = user[i].get(k);
-                if (term.equalsIgnoreCase(key))
+        for (ArrayList<String> list : user.table) {
+            for(String word : list) {
+                if (term.equalsIgnoreCase(word))
                     result++;
             }
         }
-        return result / user.length;
+        return result / user.space;
     }
-
-    public double idf(Hash[] docs, String term) {
+    //this method is for the array of control wikipedia pages
+    public double idf(Hash doc, String term) {
         double n = 0;
-        for (Hash doc : docs) {
-            for (Object word : doc) {
-                if (term.equalsIgnoreCase((String) word)) {
-                    n++;
-                    break;
+        for (ArrayList<String> list : doc.table) {
+            for (String word : list){
+                if (term.equalsIgnoreCase(word)) {
+                        n++;
+
                 }
             }
         }
-        return Math.log(docs.length / n);
+        //makes sure n isn't 0 so there isn't a divide by 0
+        if(n == 0) {
+            return 0;
+        }
+
+        return Math.log(doc.space / n);
     }
-    public double tfIdf(ArrayList<String>[] doc, Hash[] docs, String term) {
+    public double tfIdf(Hash doc, Hash docs, String term) {
         return similarity(doc, term) * idf(docs, term);
 
     }
